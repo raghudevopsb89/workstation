319ab70f6e43706cc7e2ce12343de7c6dde1eb15
Raghu K <raghuk.vit@gmail.com>
2026-05-18 05:57:45 +0530

    RoboShop Code

--- main.tf
+++ main.tf
@@ -46,3 +46,35 @@ resource "null_resource" "ip_manage" {
 output "ip" {
   value = azurerm_public_ip.workstation.ip_address
 }
+
+data "azurerm_subnet" "default" {
+  name                 = "default"
+  virtual_network_name = "workstation-vnet"
+  resource_group_name  = "denmark-east-rg"
+}
+
+resource "azurerm_public_ip" "natgw" {
+  name                = "natgw-public-ip"
+  location            = "Denmark East"
+  resource_group_name = "denmark-east-rg"
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_nat_gateway" "main" {
+  name                    = "workstation-natgw"
+  location                = "Denmark East"
+  resource_group_name     = "denmark-east-rg"
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 10
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "main" {
+  nat_gateway_id       = azurerm_nat_gateway.main.id
+  public_ip_address_id = azurerm_public_ip.natgw.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "main" {
+  subnet_id      = data.azurerm_subnet.default.id
+  nat_gateway_id = azurerm_nat_gateway.main.id
+}
