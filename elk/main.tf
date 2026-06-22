f2ee854dcf74b619ed8b19bb2259a6626308286d
Raghu K <raghuk.vit@gmail.com>
2026-06-22 06:13:21 +0530

    terraform: update 1 file(s) (main.tf)

--- elk/main.tf
+++ elk/main.tf
@@ -3,3 +3,46 @@ provider "azurerm" {
 }
 
 
+resource "null_resource" "elk_vm_manage" {
+
+  depends_on = [null_resource.elk_ip_manage]
+
+  provisioner "local-exec" {
+    #when = "create" # This line is optional as it's the default
+    command = "az vm start --resource-group denmark-east-rg --name elk"
+  }
+
+
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "az vm stop --resource-group denmark-east-rg --name elk ; az vm deallocate --resource-group denmark-east-rg --name elk"
+  }
+
+}
+
+resource "azurerm_public_ip" "elk" {
+  name                = "elk-public-ip"
+  location            = "Denmark East"
+  resource_group_name = "denmark-east-rg"
+  allocation_method   = "Static"
+}
+
+resource "null_resource" "elk_ip_manage" {
+
+  depends_on = [azurerm_public_ip.elk]
+
+  provisioner "local-exec" {
+    command = "az network nic ip-config update --resource-group denmark-east-rg --nic-name elk637_z1 --name ipconfig1 --public-ip-address elk-public-ip"
+  }
+
+
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "az network nic ip-config update --resource-group denmark-east-rg --nic-name elk637_z1 --name ipconfig1 --public-ip-address null"
+  }
+
+}
+
+output "ip" {
+  value = azurerm_public_ip.elk.ip_address
+}
